9c57f8c19909635c7b309619fb925cf76b18023b
elinni <71567184+elinni@users.noreply.github.com>
2021-11-02 22:33:35 +0100

    added list with data

---
+++ oppgave9/datalist.js
@@ -0,0 +1,34 @@
+export const people = [
+  {
+    id: '1',
+    name: 'Luke Skywalker',
+    height: '172',
+    hair_color: 'blond',
+    eye_color: 'blue',
+    gender: 'male',
+  },
+  {
+    id: '2',
+    name: 'C-3PO',
+    height: '167',
+    hair_color: 'n/a',
+    eye_color: 'yellow',
+    gender: 'n/a',
+  },
+  {
+    id: '3',
+    name: 'R2-D2',
+    height: '96',
+    hair_color: 'n/a',
+    eye_color: 'red',
+    gender: 'n/a',
+  },
+  {
+    id: '4',
+    name: 'Darth Vader',
+    height: '202',
+    hair_color: 'none',
+    eye_color: 'yellow',
+    gender: 'male',
+  },
+]
